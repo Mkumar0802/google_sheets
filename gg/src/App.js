f6e40aea0d43858703976edb6f1f9573a9c6b6ae
mkumar0802 <mkumar0802@gmail.com>
2021-11-04 21:01:18 +0530

    First commit

---
+++ gg/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { Form, Container, Header } from 'semantic-ui-react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+import axios from 'axios';
+
+import './App.css';
+
+function App() {
+  const [slno, setslno] = useState('');
+  const [date, setdate] = useState('');
+  const [transactions, settransactions] = useState('');
+  const [remarks,setremarks] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const obj = { slno, date,transactions ,remarks };
+
+    axios
+      .post(
+        'https://sheet.best/api/sheets/4c116102-9122-4130-a0aa-90dcdec8fdc8',
+        obj
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  return (
+     <Container className="coin body"> 
+
+      <Header as="h2" className="header">Cash flow sheet</Header>
+      
+
+      <Form className="form">
+       
+      <div className="tx">     
+        <TextField id="outlined-basic" label="SL.NO" variant="outlined" placeholder="SL.NO" 
+            onChange={(e) => setslno(e.target.value)}> </TextField>
+      </div> 
+            
+
+     
+      <div className="tx"> 
+<TextField id="outlined-basic" label="DATE" variant="outlined" placeholder="DATE"
+            onChange={(e) => setdate(e.target.value)}> </TextField>
+        </div>    
+        
+            <div className="tx"> 
+            <TextField fullWidth label="TRANSACTIONS" id="fullWidth" placeholder="TRANSACTIONS"
+            onChange={(e) => settransactions(e.target.value)} > </TextField>
+</div>
+
+<div className="tx">    
+<TextField fullWidth label="REMARKS" id="fullWidth" placeholder="REMARKS"
+            onChange={(e) => setremarks(e.target.value)} > </TextField>
+</div>
+        <div className="button">
+        <Button variant="contained" color="success" type="submit" onClick={handleSubmit}>summit </Button>
+        </div>
+      </Form>
+    
+    </Container>
+  );
+}
+
+
+
+
+
+
+export default App;
